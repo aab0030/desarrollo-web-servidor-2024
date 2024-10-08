4a5f8638dc3aa796e9b4fc8ad1eabdd2f637fb11
aab0030 <aab0030@alu.medac.es>
2024-10-08 14:51:06 +0200

    Ejercicio casa

---
+++ 04_Formularios/maximo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--
+    CREAR CON NÚMEROS ALETORIOS UN ARRAY CON 10 ENTEROS DEL 1 AL 50
+
+    MOSTRAR DICHOS NÚMEROS DE LA FORMA QUE MAS OS GUSTE
+
+    CREAR UN FORMULARIO DONDE SE INTENTE INTRODUCIR EL MÁXIMO VALOR Y SE COMPRUEBE SI HAS ACERTADO
+-->
+    <?php
+        $array = [
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50),
+            rand(1,50)
+        ];
+    ?>
+</body>
+</html>
