784c77f8737f60a664b8e27bb1e18104bd9cc75f
aab0030 <aab0030@alu.medac.es>
2024-11-05 12:55:55 +0100

    Toma manmcptn

---
+++ 04_Formularios/boostrap.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Formulario</h1>
+    
+        <form class="col-6" action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">Titulo</label>
+                <input class="form-control" type="text">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Descripcion</label>
+                <textarea class="form-control"></textarea>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Consola</label>
+                <div class="form-check">
+                    <input type="text" class="form-check-input">
+                    <label class="form-check-label">Play 4</label>
+                </div>
+                <div class="form-check">
+                    <input type="text" class="form-check-input">
+                    <label class="form-check-label">Play 5</label>
+                </div>
+                <div class="form-check">
+                    <input type="text" class="form-check-input">
+                    <label class="form-check-label">Switch</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
