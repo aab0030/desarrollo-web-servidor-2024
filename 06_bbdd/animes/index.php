ec56ad707132d92d364146a55a05ee1b9554558b
aab0030 <aab0030@alu.medac.es>
2024-11-19 13:40:12 +0100

    bbdd

--- 06_bbdd/animes/index.php
+++ 06_bbdd/animes/index.php
@@ -4,22 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php
         error_reporting( E_ALL );
-        ini_set( "display_errors", 1 );
-
-        require("../conexiones.php");
-    ?> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        ini_set( "display_errors", 1 ); 
+        
+        require('conexion.php');
+    ?>
+    <style>
+        .table-primary {
+            --bs-table-bg: #b0008e;
+            color: white;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <?php
-        $sql = "SELECT * FROM animes";
-        $resultado = $_conexion -> query($sql);
+            $sql = "SELECT * FROM animes";
+            $resultado = $_conexion -> query($sql);
         ?>
-        <table class=" col-8 table table-striped">
-            <thead>
+        <table class="table table-striped">
+            <thead class="table-primary">
                 <tr>
                     <th>Título</th>
                     <th>Estudio</th>
@@ -28,27 +34,20 @@
                 </tr>
             </thead>
             <tbody>
-                <?php 
-                    while($fila = $resultado -> fetch_assoc()){
-                        ?>
-                        <tr>
-                            <td><?php echo $fila["titulo"] ?></td>
-                            <td><?php echo $fila["nombre_estudio"] ?></td>
-                            <td><?php echo $fila["anno_estreno"] ?></td>
-                            <td><?php echo $fila["num_temporadas"] ?></td>
-                            <?php
-                            if ($fila["unidades_vendidas"] === NULL) {
-                                echo "No hay datos";
-                            }else echo $fila["unidades vendidas"];
-                            ?>
-                        </tr>
-                    <?php
+                <?php
+                    while($fila = $resultado -> fetch_assoc()) {
+                        // ["titulo"=>"Frieren","nombre_estudio"="Pierrot"...]
+                        echo "<tr>";
+                        echo "<td>" . $fila["titulo"] . "</td>";
+                        echo "<td>" . $fila["nombre_estudio"] . "</td>";
+                        echo "<td>" . $fila["anno_estreno"] . "</td>";
+                        echo "<td>" . $fila["num_temporadas"] . "</td>";
+                        echo "</tr>";
                     }
                 ?>
             </tbody>
         </table>
     </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
